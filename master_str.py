5123c420d690b5b2a7ac035dae44b722b4839143
Wilkens Badio <wbadio693@gmail.com>
2023-11-02 20:57:45 -0500

    commit 2

--- master_str.py
+++ master_str.py
@@ -14,3 +14,29 @@ print(lis)
 c = "alo la polis  map vini"
 n = " ".join(mot.capitalize() for mot in c.split())
 print(n)
+
+#exo 4
+q = ("alo la polis m ap vini.")
+m = q.split()
+u = ''
+for mo in m:
+    if mo:
+        p = mo[0]
+        u += p
+print("4: ",u)
+
+#exo 5
+o = "alo la polis m ap vini."
+j = o.replace("a","@")
+print("5: ",j)
+
+#exo 6
+r = "Badio"
+s = ''.join(reversed(r.upper()))
+print("6: ",s)
+
+#exo 7
+t = "alo la polis m ap vini."
+v ="a"
+k = t.count(v)
+print("7: ", k)
